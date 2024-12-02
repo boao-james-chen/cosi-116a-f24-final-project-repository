48d7bcd643d79e3e3ea76e6b94a602d3c4bf3029
James Chen <bc526@cornell.edu>
2024-12-01 23:31:26 -0500

    add a basic map

--- js/visualization.js
+++ js/visualization.js
@@ -11,6 +11,22 @@
       // .selectionDispatcher(d3.dispatch(dispatchString))
       ("#table", data);
 
+    //  map visualization
+    // adapted from bluebikes example
+    let svg = d3.select("#spatial")
+      .append("svg")
+      .attr("width", 825) 
+      .attr("height", 951);
+
+    svg.append("image")
+      .attr("xlink:href", "js/boston_map.png") 
+      .attr("width", 825) 
+      .attr("height", 951) 
+      .on("error", () => {
+        console.error("image loading failed ");
+      });
+  
+
 
   });
 
