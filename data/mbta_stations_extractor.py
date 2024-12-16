fb67bedcba2ece85e00bda3fc0c40ab7925644c6
Xenapte <17661805+Xenapte@users.noreply.github.com>
2024-12-16 12:10:45 -0500

    replace "boston" with its constituent neighborhoods

--- data/mbta_stations_extractor.py
+++ data/mbta_stations_extractor.py
@@ -1,4 +1,11 @@
 import requests
+import json
+from sys import argv
+
+if len(argv) <= 2:
+  neighborhood_file = "neighborhood_stations.json"
+else:
+  neighborhood_file = argv[1]
 
 MBTA_API = "https://api-v3.mbta.com"
 
@@ -8,13 +15,22 @@ print("Downloaded", len(routes["data"]), "route(s)")
 lines = [i["id"] for i in filter(lambda i: 0 <= i["attributes"]["type"] <= 1, routes["data"])]
 print("Found", len(lines), "T line(s):", lines)
 
+with open(neighborhood_file, "r") as neighborhood_file:
+  neighborhoods = json.load(neighborhood_file)
+
 output = "Name,Municipality,Line,Latitude,Longitude\n"
 
 for line in lines:
   stations = requests.get(f"{MBTA_API}/stops?filter[route]={line}").json()
   print("Downloaded", len(stations["data"]), "station(s) from the", line, "Line")
   for station in stations["data"]:
-    output += f"{station['attributes']['name']},{station['attributes']['municipality']},{line},{station['attributes']['latitude']},{station['attributes']['longitude']}"
+    station_name = station['attributes']['name']
+    line_equivalent = line if line != "Mattapan" else "Red"
+    for neighborhood, data in neighborhoods.items():
+      if station_name in data[line_equivalent]:
+        station["attributes"]["municipality"] = neighborhood
+      
+    output += f"{station_name},{station['attributes']['municipality']},{line},{station['attributes']['latitude']},{station['attributes']['longitude']}"
     output += "\n"
 
 with open("mbta_stations.csv", "w") as output_file:
